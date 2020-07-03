353fc4b6f175dc41ff2d67cb8fde4c36d56d3e2b
JustPandaEver <63947823+JustPandaEver@users.noreply.github.com>
2020-07-03 16:12:26 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 mkdir /data
 mkdir /data/data
 mkdir /data/data/com.termux
@@ -6,7 +7,9 @@ mkdir /data/data/com.termux/files
 mkdir /data/data/com.termux/files/usr/
 apt update -y && apt install python3.7 -y
 apt install python3-pip -y
-python3 -m pip install pyarmor
+python3.7 -m pip install pyarmor
+python3 -m pip install requests
+python3 -m pip install colorama
 echo 'import requests' > 1.py
 pyarmor o 1.py
 cp dist/pytransform /usr/lib/python3.7/
@@ -14,4 +17,5 @@ rm -rf dist
 rm -rf run.sh
 mv run1.sh run.sh
 rm -rf vps.sh
+clear
 echo 'okey your installation is done'
